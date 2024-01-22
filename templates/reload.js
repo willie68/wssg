3612da74da31e6ef559f982fd0db904dd89fcdbd
Wilfried Klaas <w.klaas@gmx.de>
2024-01-22 09:25:55 +0100

    new reload script with less console output

--- templates/reload.js
+++ templates/reload.js
@@ -2,20 +2,15 @@ function checkReload() {
   const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-  let res = this.responseText;
-  obj = JSON.parse(res);
-  if (obj["refresh"]) {
-  console.log("page has to be refeshed");
-  window.location.reload()
-  } else {
-  console.log("no refesh needed");
-  }
-  console.log(res);
-      console.log("reload answer");
-}
+      let obj = JSON.parse(this.responseText);
+      if (obj["refresh"]) {
+        console.log("page has to be refeshed");
+        window.location.reload()
+      }
+    }
   };
   xhttp.open("GET", "/_refresh", true);
   xhttp.send(); 	  
-console.log("reload");
 } 
-window.setInterval(checkReload, 5000)
+
+window.setInterval(checkReload, 1000)
